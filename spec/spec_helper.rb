da7e09395a41e10796aa85c66f53418b03edfc5f
Odin Dutton <odindutton@gmail.com>
2017-06-13 12:40:28 +1000

    Disable ruby warnings in RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,6 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.disable_monkey_patching!
-  config.warnings = true
   config.order = :random
   Kernel.srand config.seed
 end
